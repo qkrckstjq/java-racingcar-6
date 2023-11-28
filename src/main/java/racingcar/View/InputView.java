047ee8ba3a2cc13da2037856189b6458dc6d4781
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-11-28 15:39:20 +0900

    feat InputView, OutputView 작성

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -1,4 +1,13 @@
 package racingcar.View;
 
+import camp.nextstep.edu.missionutils.Console;
 public class InputView {
+    public static String printInputCars () {
+        System.out.println(DefaultMessage.START_MESSAGE.getMessage());
+        return Console.readLine();
+    }
+    public static String printInputNumber () {
+        System.out.println(DefaultMessage.ASK_NUMBER_MESSAGE.getMessage());
+        return Console.readLine();
+    }
 }
--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,7 +1,11 @@
 package racingcar.View;
 
 public class OutputView {
-    public static void printStart () {
-        System.out.println();
+    public static void printResultMessage () {
+        System.out.println(DefaultMessage.RUN_RESULT_MESSAGE.getMessage());
+    }
+
+    public static void printResult (String name, String progress) {
+        System.out.printf("%s : %s\n", name, progress);
     }
 }
